95db10bf102ba9f4e01779a39e5cbb3325cc6ea0
zhangjie <zx.zhangjie@qq.com>
2017-02-23 20:17:21 +0800

    copy..

--- math/data-convert.c
+++ math/data-convert.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
-
+#include <string.h>
+//c
 int main(){
 	int a,b;
 	char n[20];
 	while(scanf("%d %s %d",&a,n,&b)!=EOF){
-		char tmp[20];
-		int c=1;
-		for(int i=0;n[i]!=0;i++){
+		int tmp=0,lenth=strlen(n),c=1;
+		for(int i=lenth-1;i>=0;i--){
+			int x;
 			if(n[i]>='0'&&n[i]<='9'){
-				tmp[i]=n[i];
+				x=n[i]-'0';
 			}else if(n[i]>='a'&&n[i]<='z'){
-				tmp[i]=n[i]-'a'+10;
-			}else tmp[i]=n[i]-'A'+10;
+				x=n[i]-'a'+10;
+			}else x=n[i]-'A'+10;
+			tmp+=x*c;
+			c*=a;
+		}//convert to ten
+		char ans[40],size=0;
+		do{
+			int x=tmp%b;
+			ans[size++]=(x<10) ? x+'0' : x-10+'A';
+			tmp/=b;
+		}while(tmp);
+		//convert to b
+		for(int i=size-1;i>=0;i--){
+			printf("%c",ans[i]);
 		}
-		for(int i=0;tmp[i]!=0;i++)
-			printf("%c",tmp[i]);
+		printf("\n");
 	}
 	return 0;
 }
