1058d87eaa254ebec97712a82dd58a09c3ea0689
zhangjie <zx.zhangjie@qq.com>
2017-02-23 19:22:58 +0800

    today's work

--- math/new-ab.c
+++ math/new-ab.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(){
-	int a,b,m;
+	long long a,b;
+	int m;
 	while(scanf("%d",&m)!=EOF&&m!=0){
-		scanf("%d %d",&a,&b);
-		long long ans=0;
-		ans=a+b;
+		scanf("%lld %lld",&a,&b);
+		a=a+b;
 		int idx=0;
-		char num[20];
-		while(ans!=0){
-			num[idx++]=ans%m;
-			ans/=m;
-		}
+		char num[50];
+		do{
+			num[idx++]=a%m;
+			a/=m;
+		}while(a!=0);
 		num[idx]=0;
 		for(int i=idx-1;i>=0;i--){
 			printf("%d",num[i]);
