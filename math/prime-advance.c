a0245faa8a687c0eee11f062b035e50dd71199fa
zhangjie <zx.zhangjie@qq.com>
2017-02-24 09:38:28 +0800

    prime-advance

---
+++ math/prime-advance.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+bool mark[10000];
+
+bool judge(int x){
+	if(x<=1) return false;
+	int bound=(int)sqrt(x)+1;
+	for(int i=2;i<bound;i++){
+		if(x%i==0) return false;
+	}
+	return true;
+}
+void init(){
+	int c=1;
+	for(int i=2;i<=10000;i++){
+		mark[i]=judge(i);
+		if(mark[i]){
+			//i is prime
+			while(i*c<=10000){
+				mark[i*c]=false;
+				c++;
+			}
+		}
+	}
+}
+
+int main(){
+	int n,k=0;
+	init();
+	while(scanf("%d",&n)!=EOF){
+		for(int i=0;i<n;i++){
+			if(mark[i]&&i%10==1){
+				printf("%d ",i);
+				k=1;
+			}
+		}
+		if(k==0) printf("-1");
+
+		printf("\n");
+	}
+	return 0;
+}
