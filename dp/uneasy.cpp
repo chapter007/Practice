2cd602864677c14c1107e0d982273fc17f9b8f7d
zhangjie <zx.zhangjie@qq.com>
2017-03-03 15:03:11 +0800

    today's work

--- dp/uneasy.cpp
+++ dp/uneasy.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
+	int n;
+	long long F[100];
+	F[1]=0;F[2]=1;
+	for(int i=3;i<=100;i++){
+		F[i]=(i-1)*F[i-1]+(i-1)*F[i-2];
+	}
+	while(scanf("%d",&n)!=EOF){
+		printf("%lld\n",F[n]);
+	}
 	return 0;
 }
