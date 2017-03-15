e76a353d2624d17484c3d0b63ddfe68c49f17870
zhangjie <zx.zhangjie@qq.com>
2017-03-15 21:06:46 +0800

    practice

---
+++ greed/miz.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int c;
+	scanf("%d",&c);
+	while(c--){
+		int n,V,W,pi[100];
+		scanf("%d %d %d",&n,&V,&W);
+		for(int i=0;i<n;i++){
+			scanf("%d",&pi[i]);	
+		}
+	}
+	return 0;
+}
